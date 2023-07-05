79b002d81273118f2c2b6fa71d5411b9d73c0f22
Kayden Grant <hello@kaydengrant.io>
2023-07-05 00:33:25 -0700

    fixed sanity env and added sanity token

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,19 +7,18 @@ import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import { apiVersion, dataset, projectId } from './sanity/env';
 import { schema } from './sanity/schemas';
 
 export default defineConfig({
   basePath: '/studio',
-  projectId,
-  dataset,
+  projectId: 'v9y56en8',
+  dataset: 'production',
   // Add and edit the content schema in the './sanity/schema' folder
   schema,
   plugins: [
     deskTool(),
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({ defaultApiVersion: apiVersion }),
+    visionTool({ defaultApiVersion: '2023-07-01' }),
   ],
 });
--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,10 +1,18 @@
-import { createClient } from 'next-sanity'
+import { createClient } from 'next-sanity';
 
-import { apiVersion, dataset, projectId, useCdn } from '../env'
+import createImageUrlBuilder from '@sanity/image-url';
+import { SanityImageSource } from '@sanity/image-url/lib/types/types';
 
 export const client = createClient({
-  apiVersion,
-  dataset,
-  projectId,
-  useCdn,
-})
+  projectId: 'v9y56en8',
+  dataset: 'production',
+  apiVersion: '2023-07-01',
+  useCdn: true,
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+});
+
+const imageBuilder = createImageUrlBuilder(client);
+
+export const urlForImage = (source: any) => {
+  return imageBuilder?.image(source).auto('format').fit('max');
+};
