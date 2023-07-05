d2b0e49866e90ea7185c1a10e6c844f222ccf44c
Kayden Grant <hello@kaydengrant.io>
2023-07-05 00:34:37 -0700

    exported all components in index file

---
+++ src/components/index.tsx
@@ -0,0 +1,7 @@
+export { default as Footer } from './Footer';
+export { default as ShopFooter } from './ShopFooter';
+export { default as OutlineButton } from './OutlineButton';
+export { default as BlogNav } from './navigation/BlogNav';
+export { default as ShopNav } from './navigation/ShopNav';
+export { default as NavBar } from './navigation/NavBar';
+export { default as TitleBanner } from './banners/TitleBanner';
