9ab4a9aa725717cc9bc77c6018e72b765e8fe7ce
Kayden Grant <hello@kaydengrant.io>
2023-08-14 14:45:41 -0700

    added custom 404 page (not styled)

---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Custom404 = () => {
+  return (
+    <div className="absolute inset-0 flex flex-col z-30 items-center justify-center bg-white">
+      <h1>This page does not exist - 404</h1>
+      <h1>This page does not exist - 404</h1>
+      <h1>This page does not exist - 404</h1>
+      <h1>This page does not exist - 404</h1>
+      <h1>This page does not exist - 404</h1>
+      <h1>This page does not exist - 404</h1>
+    </div>
+  );
+};
+
+export default Custom404;
