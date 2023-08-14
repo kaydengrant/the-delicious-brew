59191856cca3da2e5de7e8a0f310371bdddf111e
Kayden Grant <hello@kaydengrant.io>
2023-08-14 14:46:24 -0700

    added sales tax and functionality to buttons

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import React, { useRef } from 'react';
+import Link from 'next/link';
 
 import { CreditCard, RemoveShoppingCart } from '../../utils/icons';
 import { CartItem, OutlineButton } from '../../components';
@@ -8,6 +9,7 @@ import { addCommasToNumber } from '../../utils';
 
 const Cart: React.FC = () => {
   const { totalPrice, totalQuantities, cartItems } = useStateContext();
+  const salesTax = parseFloat(((6.25 / 100) * totalPrice).toFixed(2));
 
   return (
     <section>
@@ -17,10 +19,12 @@ const Cart: React.FC = () => {
         <section className="flex flex-col justify-center items-center gap-4">
           <RemoveShoppingCart size={150} />
           <h4>Your cart is empty</h4>
-          <OutlineButton text="Continue Shopping" fillContainer />
+          <Link href={'/shop'}>
+            <OutlineButton text="Continue Shopping" fillContainer />
+          </Link>
         </section>
       ) : (
-        <>
+        <div className="flex flex-col items-center">
           {cartItems.map((item, index) => (
             <CartItem
               key={item._id}
@@ -41,17 +45,23 @@ const Cart: React.FC = () => {
                 <p>${addCommasToNumber(totalPrice)}</p>
               </div>
               <div className="flex flex-row justify-between">
+                <p>Sales tax</p>
+                <p>${addCommasToNumber(salesTax)}</p>
+              </div>
+              <div className="flex flex-row justify-between">
                 <p>Shipping</p>
                 <p>FREE</p>
               </div>
               <div className="bg-gray h-0.5" />
               <div className="flex flex-row justify-between">
                 <h4>Order Total</h4>
-                <h4>${addCommasToNumber(totalPrice)}</h4>
+                <h4>${addCommasToNumber(totalPrice + salesTax)}</h4>
               </div>
               <div className="bg-gray h-0.5" />
               <div className="flex flex-col md:flex-row justify-around self-center gap-2 md:gap-4 px-4 w-[300px] md:w-full">
-                <OutlineButton text="Continue Shopping" fillContainer />
+                <Link href={'/shop'} className="w-full">
+                  <OutlineButton text="Continue Shopping" fillContainer />
+                </Link>
                 <OutlineButton
                   text="Pay With Stripe"
                   Icon={CreditCard}
@@ -60,7 +70,7 @@ const Cart: React.FC = () => {
               </div>
             </div>
           </div>
-        </>
+        </div>
       )}
     </section>
   );
