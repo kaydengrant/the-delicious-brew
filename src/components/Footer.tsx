f0ed6689c68c6ee63acbced10f58c9fa58decd15
Kayden Grant <hello@kaydengrant.io>
2023-08-17 17:18:17 -0700

    fixed footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <>
+    <div>
       <div className=" bg-gray h-0.5" />
       <div className="flex flex-col justify-center w-full py-2">
         <p className="flex flex-row justify-end px-2">
           © 2023 Kayden Grant. All Rights Reserved.
         </p>
       </div>
-    </>
+    </div>
   );
 };
 
