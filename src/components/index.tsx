fb8b9095a8967b4944e7ae7a107eaff7bc0d3e54
Kayden Grant <hello@kaydengrant.io>
2023-08-17 17:16:31 -0700

    updated exports

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,22 +3,19 @@ export { default as Divider } from './Divider';
 export { default as Carousel } from './Carousel';
 export { default as ProductSpecifications } from './ProductSpecifications';
 export { default as CartItem } from './CartItem';
-
+export { default as QuickAddToCartModal } from './QuickAddToCartModal';
 export { default as NavBlog } from './navigation/NavBlog';
 export { default as NavShop } from './navigation/NavShop';
 export { default as NavMobile } from './navigation/NavMobile';
 export { default as NavBar } from './navigation/NavBar';
 export { default as NavFooter } from './navigation/NavFooter';
 export { default as NavLink } from './navigation/NavLink';
-
 export { default as TitleBanner } from './banners/TitleBanner';
 export { default as ProductBanner } from './banners/ProductBanner';
 export { default as VerticalBanner } from './banners/VerticalBanner';
 export { default as LabelBanner } from './banners/LabelBanner';
-
 export { default as OutlineButton } from './buttons/OutlineButton';
 export { default as DropDownButton } from './buttons/DropDownButton';
 export { default as QuantityButton } from './buttons/QuantityButton';
 export { default as DonationButton } from './buttons/DonationButton';
-
 export { default as ToastProvider } from './ToastProvider';
