4070b7253048073cedd17d52c73db88adbfd9a79
Kayden Grant <hello@kaydengrant.io>
2023-08-17 21:52:11 -0700

    added cart items to payment page; updated text

--- src/app/cart/payment/page.tsx
+++ src/app/cart/payment/page.tsx
@@ -1,16 +1,22 @@
 'use client';
 import React from 'react';
+import Image from 'next/image';
+import Marquee from 'react-fast-marquee';
 
 import { OutlineButton, DonationButton } from '../../../components';
 import { MugHot } from '../../../utils/icons';
+import { useStateContext } from '../../../context/StateContext';
+import { urlForImage } from '../../../../sanity/lib/client';
 
-const Payment = () => {
+const Payment: React.FC = () => {
+  const { cartItems } = useStateContext();
+
   return (
     <section className="flex flex-col items-center text-center gap-4">
       <h1>Sorry, this is only a portfolio project.</h1>
       <h3>
-        While the items are real, you cannot purchase them through
-        TheDeliciousBrew.com
+        While the items are real, you cannot purchase them through The Delicious
+        Brew
       </h3>
       <p>
         If you are still interested in purchasing the product, I recommend
@@ -24,6 +30,82 @@ const Payment = () => {
         </DonationButton>
         <DonationButton />
       </div>
+      <h3 className="items-start mt-10">Your Cart Item(s)</h3>
+      {cartItems.length > 1 ? (
+        <Marquee autoFill>
+          {cartItems.map((item: any, index: number) => {
+            const img = urlForImage(item.image[0]).width(1000).url();
+
+            return (
+              <div
+                key={item._id}
+                className="flex flex-col gap-2 max-w-[200px] mx-2"
+              >
+                <div
+                  className={`flex relative rounded-xl w-[200px] h-[200px] min-w-[200px] min-h-[200px] p-2 drop-shadow-sm ${
+                    index % 3 == 0
+                      ? 'bg-blue'
+                      : index % 3 == 1
+                      ? 'bg-green'
+                      : 'bg-brown'
+                  }`}
+                >
+                  <Image
+                    loader={() => img}
+                    src={img}
+                    alt="Product banner image"
+                    className="drop-shadow-xl object-contain"
+                    fill
+                    unoptimized
+                    priority
+                  />
+                </div>
+                <div className="flex flex-col text-start">
+                  <p className="truncate">{item.name}</p>
+                  <p className="font-semibold">${item.price}</p>
+                </div>
+              </div>
+            );
+          })}
+        </Marquee>
+      ) : (
+        <div className="flex flex-row">
+          {cartItems.map((item: any, index: number) => {
+            const img = urlForImage(item.image[0]).width(1000).url();
+
+            return (
+              <div
+                key={item._id}
+                className="flex flex-col gap-2 max-w-[200px] mx-2"
+              >
+                <div
+                  className={`flex relative rounded-xl w-[200px] h-[200px] min-w-[200px] min-h-[200px] p-2 drop-shadow-sm ${
+                    index % 3 == 0
+                      ? 'bg-blue'
+                      : index % 3 == 1
+                      ? 'bg-green'
+                      : 'bg-brown'
+                  }`}
+                >
+                  <Image
+                    loader={() => img}
+                    src={img}
+                    alt="Product banner image"
+                    className="drop-shadow-xl object-contain"
+                    fill
+                    unoptimized
+                    priority
+                  />
+                </div>
+                <div className="flex flex-col text-start">
+                  <p className="truncate">{item.name}</p>
+                  <p className="font-semibold">${item.price}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </section>
   );
 };
